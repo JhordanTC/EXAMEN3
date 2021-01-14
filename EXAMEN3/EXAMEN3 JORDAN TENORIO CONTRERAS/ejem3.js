3131b3de443752bd175e205ed7b76454a6009a40
JhordanTC <77450686+JhordanTC@users.noreply.github.com>
2021-01-14 11:38:13 -0500

    Add files via upload

---
+++ EXAMEN3/EXAMEN3 JORDAN TENORIO CONTRERAS/ejem3.js
@@ -0,0 +1,45 @@
+var moda=new Array();
+var mod1=new Object();
+mod1.nombre="Polera ryan";
+
+var mod2=new Object();
+mod2.nombre="Conjunto RIPNDIP";
+
+var mod3=new Object();
+mod3.nombre="Polo levis";
+
+var mod4=new Object();
+mod4.nombre="Jean DC rasgado";
+
+var mod5=new Object();
+mod5.nombre="Nike Jordan onen";
+
+var mod6=new Object();
+mod6.nombre="Nike AIR force one";
+
+var mod7=new Object();
+mod7.nombre="Gorra RIPNDIP";
+
+var mod8=new Object();
+mod8.nombre="Corta vientos DC";
+
+var mod9=new Object();
+mod9.nombre="Short Gucci";
+
+var mod10=new Object();
+mod10.nombre="Sudadera Supreme";
+
+moda.push(mod1);
+moda.push(mod2);
+moda.push(mod3);
+moda.push(mod4);
+moda.push(mod5);
+moda.push(mod6);
+moda.push(mod7);
+moda.push(mod8);
+moda.push(mod9);
+moda.push(mod10);
+
+function mostrar1(){
+    alert(moda[9].nombre);
+}
---
+++ EXAMEN3/EXAMEN3 JORDAN TENORIO CONTRERAS/ejem4.js
@@ -0,0 +1,198 @@
+var vehiculos=new Array();
+var carro1=new Object();
+carro1.nombre="Leon";
+carro1.costo="339.000$";
+carro1.marca="ferrari";
+carro1.velocidadmax="340km";
+carro1.tiempodevelocidad="2.9seg";
+carro1.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+var carro2=new Object();
+carro2.nombre="Giulietta";
+carro2.costo="339.000$";
+carro2.marca="ferrari";
+carro2.velocidadmax="340km";
+carro2.tiempodevelocidad="2.9seg";
+carro2.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+var carro3=new Object();
+carro3.nombre="MiTo";
+carro3.costo="339.000$";
+carro3.marca="ferrari";
+carro3.velocidadmax="340km";
+carro3.tiempodevelocidad="2.9seg";
+carro3.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+var carro4=new Object();
+carro4.nombre="Giulia";
+carro4.costo="339.000$";
+carro4.marca="ferrari";
+carro4.velocidadmax="340km";
+carro4.tiempodevelocidad="2.9seg";
+carro4.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+var carro5=new Object();
+carro5.nombre="Stelvio";
+carro5.costo="339.000$";
+carro5.marca="ferrari";
+carro5.velocidadmax="340km";
+carro5.tiempodevelocidad="2.9seg";
+carro5.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+var carro6=new Object();
+carro6.nombre="DB9";
+carro6.costo="339.000$";
+carro6.marca="ferrari";
+carro6.velocidadmax="340km";
+carro6.tiempodevelocidad="2.9seg";
+carro6.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+var carro7=new Object();
+carro7.nombre="Vantage V8";
+carro7.costo="339.000$";
+carro7.marca="ferrari";
+carro7.velocidadmax="340km";
+carro7.tiempodevelocidad="2.9seg";
+carro7.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro8=new Object();
+carro8.nombre="Vantage V12";
+carro8.costo="339.000$";
+carro8.marca="ferrari";
+carro8.velocidadmax="340km";
+carro8.tiempodevelocidad="2.9seg";
+carro8.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro9=new Object();
+carro9.nombre="ASTON MARTIN";
+carro9.costo="339.000$";
+carro9.marca="ferrari";
+carro9.velocidadmax="340km";
+carro9.tiempodevelocidad="2.9seg";
+carro9.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro10=new Object();
+carro10.nombre="AUDI";
+carro10.costo="339.000$";
+carro10.marca="ferrari";
+carro10.velocidadmax="340km";
+carro10.tiempodevelocidad="2.9seg";
+carro10.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro11=new Object();
+carro11.nombre="A6 allroad quattro";
+carro11.costo="339.000$";
+carro11.marca="ferrari";
+carro11.velocidadmax="340km";
+carro11.tiempodevelocidad="2.9seg";
+carro11.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro12=new Object();
+carro12.nombre="RS5";
+carro12.costo="339.000$";
+carro12.marca="ferrari";
+carro12.velocidadmax="340km";
+carro12.tiempodevelocidad="2.9seg";
+carro12.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro13=new Object();
+carro13.nombre="SQ5";
+carro13.costo="339.000$";
+carro13.marca="ferrari";
+carro13.velocidadmax="340km";
+carro13.tiempodevelocidad="2.9seg";
+carro13.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro14=new Object();
+carro14.nombre="RS Q3";
+carro14.costo="339.000$";
+carro14.marca="ferrari";
+carro14.velocidadmax="340km";
+carro14.tiempodevelocidad="2.9seg";
+carro14.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro15=new Object();
+carro15.nombre="Bentayga";
+carro15.costo="339.000$";
+carro15.marca="ferrari";
+carro15.velocidadmax="340km";
+carro15.tiempodevelocidad="2.9seg";
+carro15.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro16=new Object();
+carro16.nombre="TTS";
+carro16.costo="339.000$";
+carro16.marca="ferrari";
+carro16.velocidadmax="340km";
+carro16.tiempodevelocidad="2.9seg";
+carro16.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro17=new Object();
+carro17.nombre="BENTLEY";
+carro17.costo="339.000$";
+carro17.marca="ferrari";
+carro17.velocidadmax="340km";
+carro17.tiempodevelocidad="2.9seg";
+carro17.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro18=new Object();
+carro18.nombre="Continental GT";
+carro18.costo="339.000$";
+carro18.marca="ferrari";
+carro18.velocidadmax="340km";
+carro18.tiempodevelocidad="2.9seg";
+carro18.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro19=new Object();
+carro19.nombre="Mulsanne";
+carro19.costo="339.000$";
+carro19.marca="ferrari";
+carro19.velocidadmax="340km";
+carro19.tiempodevelocidad="2.9seg";
+carro19.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+
+var carro20=new Object();
+carro20.nombre="Flying Spur";
+carro20.costo="339.000$";
+carro20.marca="ferrari";
+carro20.velocidadmax="340km";
+carro20.tiempodevelocidad="2.9seg";
+carro20.tamaño="Longitud:4.594mm Ancho:1.938mm";
+
+vehiculos.push(carro1);
+vehiculos.push(carro2);
+vehiculos.push(carro3);
+vehiculos.push(carro4);
+vehiculos.push(carro5);
+vehiculos.push(carro6);
+vehiculos.push(carro7);
+vehiculos.push(carro8);
+vehiculos.push(carro9);
+vehiculos.push(carro10);
+vehiculos.push(carro11);
+vehiculos.push(carro12);
+vehiculos.push(carro13);
+vehiculos.push(carro14);
+vehiculos.push(carro15);
+vehiculos.push(carro16);
+vehiculos.push(carro17);
+vehiculos.push(carro18);
+vehiculos.push(carro19);
+vehiculos.push(carro20);
+
+function mostrar(){
+    alert(vehiculos[1].nombre);
+}
